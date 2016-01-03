0d2dd0df00e40b066761570cd136a2c2cfb6a1cf
Den <trub.denchik@gmail.com>
2016-01-03 17:52:35 +0300

    Update m140622_111540_create_image_table.php
    I have some problem when update migration for postgresql db

--- migrations/m140622_111540_create_image_table.php
+++ migrations/m140622_111540_create_image_table.php
@@ -8,11 +8,11 @@ class m140622_111540_create_image_table extends \yii\db\Migration
     {
         $this->createTable('image', [
             'id' => 'pk',
-            'filePath' => 'VARCHAR(400) NOT NULL',
-            'itemId' => 'int(20) NOT NULL',
-            'isMain' => 'int(1)',
-            'modelName' => 'VARCHAR(150) NOT NULL',
-            'urlAlias' => 'VARCHAR(400) NOT NULL',
+            'filePath' => $this->string(400)->notNull(),
+            'itemId' => $this->integer(20)->notNull(),
+            'isMain' => $this->integer(1),
+            'modelName' => $this->string(150)->notNull(),
+            'urlAlias' => $this->string(400)->notNull(),
         ]);
 
     }
